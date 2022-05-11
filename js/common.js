04ac41cad2b58a59e2bbf928a312cbddc58b23b1
shchocho <95754270+shchocho@users.noreply.github.com>
2022-05-11 17:38:22 +0900

    CODE -DAY(2)

--- js/common.js
+++ js/common.js
@@ -1,6 +1,37 @@
-var swiper = new Swiper("#mainbanner",{
-  loop:true,
-  autoplay:{
-    delay: 4000
-  }
+$(function(){
+  $(window).scroll(function(){
+    let hTop = $("#header").offset().top;
+     if(hTop > 0){
+       $("#header").addClass("contents-hd");
+     }else if( hTop == 0){
+       $("#header").removeClass("contents-hd");
+     }
+
+     if(hTop > 1000){
+       $(".txt-wrap").addClass("act")
+        $(".btn").addClass("actbt")
+     }
+  })
+
+
+  $(".banner-item:last-child").addClass("opa");
+
+  setInterval(function(){
+    $(".banner-item:last-child").prependTo(".banner-wrap");
+    $(".banner-item").removeClass("opa");
+    $(".banner-item:last-child").addClass("opa");
+  },6000)
+
+  $(".dot").click(function(){
+    $(".dot").removeClass("pick")
+    $(this).addClass("pick")
+  })
+
+  $(".section-dot").click(function(){
+    $(".section-dot").removeClass("pick")
+    $(this).addClass("pick")
+  })
+
+
+
 })
