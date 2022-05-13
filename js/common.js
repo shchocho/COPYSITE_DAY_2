30eda4a5b716de3ac3e89ae88eeafa34c0f38c20
shchocho <95754270+shchocho@users.noreply.github.com>
2022-05-13 17:24:36 +0900

    CODE -DAY(4)

--- js/common.js
+++ js/common.js
@@ -1,37 +1,84 @@
-$(function(){
-  $(window).scroll(function(){
+$(function() {
+
+  let windowHeight = $(window).innerHeight();
+  let direc = 0;
+  let maxDirec = $("section").length - 1;
+  let wheelAction = false;
+
+  function wheelMove() {
+    $("html").animate({
+      scrollTop: direc * windowHeight
+    }, {
+      duration: 1000,
+      start: function() {
+        wheelAction: true;
+      },
+      complete: function() {
+        wheelAction: false;
+      }
+    })
+    if (direc == 1) {
+      $(".txt-wrap").addClass("act")
+      $(".btn").addClass("actbt")
+    }
+  }
+
+  function wheelAct(eventDelta) {
+    if (eventDelta > 0 && wheelAction == false) {
+      if (direc <= 0) {
+        direc = 0;
+      } else {
+        direc--;
+        wheelMove();
+      }
+    } else if (eventDelta < 0 && wheelAction == false) {
+      if (direc >= maxDirec) {
+        direc = maxDirec + 1;
+      } else {
+        direc++;
+        wheelMove();
+      }
+    }
+    wheelMove();
+
+
+
+  }
+
+  $(window).on("wheel", function(event) {
+    let eventDelta = event.originalEvent.wheelDelta;
+    wheelAct(eventDelta);
+  });
+  $(window).scroll(function() {
     let hTop = $("#header").offset().top;
-     if(hTop > 0){
-       $("#header").addClass("contents-hd");
-     }else if( hTop == 0){
-       $("#header").removeClass("contents-hd");
-     }
-
-     if(hTop > 1000){
-       $(".txt-wrap").addClass("act")
-        $(".btn").addClass("actbt")
-     }
-  })
-
+    if (hTop > 0) {
+      $("#header").addClass("contents-hd");
+    } else if (hTop == 0) {
+      $("#header").removeClass("contents-hd");
+    }
+  });
 
   $(".banner-item:last-child").addClass("opa");
 
-  setInterval(function(){
+  setInterval(function() {
     $(".banner-item:last-child").prependTo(".banner-wrap");
     $(".banner-item").removeClass("opa");
-    $(".banner-item:last-child").addClass("opa");
-  },6000)
+    $(".banner-item:last-child").addClass("opa");}, 6000);
 
-  $(".dot").click(function(){
+  $(".dot").click(function() {
     $(".dot").removeClass("pick")
     $(this).addClass("pick")
-  })
+  });
+  $(".section-dot").each(function(i) {
+    let thisIndex = i;
+    $(this).click(function() {
+      $(".section-dot").removeClass("pick");
+      $(this).addClass("pick");
 
-  $(".section-dot").click(function(){
-    $(".section-dot").removeClass("pick")
-    $(this).addClass("pick")
-  })
-
+      direc = thisIndex;
+      wheelMove()
+    })
+  });
 
 
 })
